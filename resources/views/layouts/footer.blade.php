8a3ff2896691822b081eefb128001db50837d838
setiya14 <setiyaa869.@gmail.com>
2024-01-31 12:58:31 +0700

    upload

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Makeby kelompok5 Thriftique Webster </span>
+    </div>
+  </div>
+</footer>
